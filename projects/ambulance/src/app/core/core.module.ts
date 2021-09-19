50a75c174b854725f74d74b7ed25a2c7d64624e2
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-09-19 13:43:26 -0500

    Layout design

--- projects/ambulance/src/app/core/core.module.ts
+++ projects/ambulance/src/app/core/core.module.ts
@@ -6,17 +6,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     PageLoginComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
@@ -25,8 +29,16 @@ import { HeaderComponent } from './components/header/header.component';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
+    MatMenuModule,
+    MatListModule,
     FlexLayoutModule
   ],
-  exports: [PageLoginComponent, HeaderComponent]
+  exports: [
+    PageLoginComponent,
+    HeaderComponent,
+    MenuComponent,
+    MatIconModule,
+    FlexLayoutModule
+  ]
 })
 export class CoreModule {}
